422dc1c7b6447ecc694356568894890266c5ff0d
José Castro <a.j.castro.b@gmail.com>
2026-08-21 17:50:45 -0500

    🚀 [ADD] Order
    Se agregó el order para publicar por los más recientes en la api

--- src/lib/wagtail.js
+++ src/lib/wagtail.js
@@ -3,7 +3,7 @@ const url_base = import.meta.env.PUBLIC_WAGTAIL_API;
 export const getBlogPost = async () => {
   try {
     const res = await fetch(
-      `${url_base}/pages/?type=skills_blog.ArticlePage&fields=*`,
+      `${url_base}/pages/?type=skills_blog.ArticlePage&fields=*&order=-first_published_at`,
     );
 
     if (!res.ok) {
