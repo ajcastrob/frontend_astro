9dc49b3741853d0f0076b5760ca3192d8aec9304
José Castro <a.j.castro.b@gmail.com>
2026-08-26 16:59:03 -0500

    🔧 [CHORE] Añadir oxlint y oxfmt

--- src/lib/wagtail.js
+++ src/lib/wagtail.js
@@ -29,9 +29,7 @@ export const imageUrl = (image) => {
   if (/^https?:\/\//.test(path)) return path;
 
   try {
-    const origin = url_base
-      ? new URL(url_base).origin
-      : "http://127.0.0.1:8000";
+    const origin = url_base ? new URL(url_base).origin : "http://127.0.0.1:8000";
     return `${origin}${path}`;
   } catch {
     return `http://127.0.0.1:8000${path}`;
